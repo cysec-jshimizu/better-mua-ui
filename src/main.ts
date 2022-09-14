0e2943cec9c5d548ad9c88beb0515657b7317f66
cysec-jshimizu <77430623+cysec-jshimizu@users.noreply.github.com>
2022-09-14 15:50:06 +0900

    for outlook

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,40 @@
-import { inbox, inMail, inSrc, inNewMail } from "./gmail";
+import * as gmail from "./gmail";
+import * as outlook from "./outlook";
 
 function mailGoogleCom(url: URL) {
   let params: URLSearchParams = url.searchParams;
 
   if (url.hash.match(/#inbox(\/p2)?$/)) {
     // メール一覧
-    inbox();
+    gmail.inbox();
   } else if (url.hash.match(/#inbox(?:\/p2)?\?compose=new/)) {
     // 新規メールを書く
-    inNewMail();
+    gmail.inNewMail();
   } else if (url.hash.match(/#inbox\/[\w]+/)) {
     // メールを展開
-    inMail();
+    gmail.inMail();
   } else if (params.has("permmsgid") && params.has("ik") && params.has("view")) {
     // メールのソースを表示
-    inSrc();
+    gmail.inSrc();
   }
 }
 
 function outlookOffice365Com(url: URL) {
-  console.log("HELLO outlook.", url);
+  outlook.inbox();
+  if (url.pathname.match(/^\/mail\/$/)) {
+    console.log("inbox");
+  } else if (url.pathname.match(/^\/mail\/inbox\/id\/[\w\%]+/)) {
+    console.log("open email");
+  }
+
+  let newEmailButton = document.querySelectorAll("#app .ms-Button--commandBar")[1];
+  newEmailButton?.addEventListener("click", () => {
+    console.log("writing a new email");
+  })
 }
 
 function main() {
   let url: URL = new URL(document.URL);
-  let params: URLSearchParams = url.searchParams;
   if (url.origin + url.pathname === "https://mail.google.com/mail/u/0/") {
     // hashが変更されたらevent
     addEventListener("hashchange", function (event: HashChangeEvent) {
@@ -33,7 +43,7 @@ function main() {
     });
 
     mailGoogleCom(url);
-  } else if (url.origin + url.pathname === "https://outlook.office365.com/mail/") {
+  } else if (url.origin === "https://outlook.office.com") {
     outlookOffice365Com(url);
   }
 }
--- src/outlook.ts
+++ src/outlook.ts
@@ -1,4 +1,158 @@
-function inSrc() {}
-function getEmailList() {}
-function inMail() {}
-function inbox() {}
+import { mailParser, parseSecStat } from "./email";
+import { sleep } from "./util";
+
+
+function inSrc() { }
+
+async function getEmailList() {
+  let _threadList: NodeListOf<HTMLElement> = document.querySelectorAll(".s0OAm>div");
+  let threadList: Array<HTMLElement> = [];
+  // threadから複数のクラスがあるもの(セパレータ)を除去
+  _threadList.forEach((ele) => {
+    if (ele.classList.length < 2) {
+      threadList.push(ele);
+    }
+  })
+
+  // get cookie
+  let x_owa_canary: string = "";
+  let cookies = document.cookie.split(";");
+  for (let c of cookies) {
+    if (c.trim().startsWith("X-OWA-CANARY")) {
+      x_owa_canary = c.trim().replace("X-OWA-CANARY=", "");
+    }
+  }
+
+  // fetch emaillist
+  let temp = await fetch("https://outlook.office.com/owa/startupdata.ashx?app=Mail", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "ja,en-US;q=0.9,en;q=0.8",
+      "action": "StartupData",
+      "folderparams": "{\"TimeZoneStr\":\"Tokyo Standard Time\"}",
+      "messageparams": "{\"TimeZoneStr\":\"Tokyo Standard Time\",\"InboxReadingPanePosition\":1,\"IsFocusedInboxOn\":true,\"BootWithConversationView\":true,\"SortResults\":[{\"Path\":{\"__type\":\"PropertyUri:#Exchange\",\"FieldURI\":\"conversation:LastDeliveryOrRenewTime\"},\"Order\":\"Descending\"},{\"Path\":{\"__type\":\"PropertyUri:#Exchange\",\"FieldURI\":\"conversation:LastDeliveryTime\"},\"Order\":\"Descending\"}]}",
+      "x-owa-canary": x_owa_canary,
+      "x-owa-host-app": "unknown",
+      "x-owa-hosted-ux": "false",
+      "x-req-source": "Mail"
+    },
+    "referrerPolicy": "no-referrer",
+    "body": null,
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+  });
+  let tempJ = await temp.json();
+
+
+  let emails = [];
+  // fetch email header
+  for (let [index, emailDetail] of Object.entries(tempJ["findConversation"]["Body"]["Conversations"])) {
+    // @ts-ignore
+    let emailId = emailDetail["GlobalItemIds"][0]["Id"];
+    let res = await fetch("https://outlook.office.com/owa/service.svc?action=GetItem&app=Mail", {
+      method: "POST",
+      headers: {
+        "action": "GetItem",
+        "content-type": "application/json; charset=utf-8",
+        "x-owa-canary": x_owa_canary,
+        "x-owa-urlpostdata": `{"__type":"GetItemJsonRequest:#Exchange","Header":{"__type":"JsonRequestHeaders:#Exchange","RequestServerVersion":"V2016_06_24","TimeZoneContext":{"__type":"TimeZoneContext:#Exchange","TimeZoneDefinition":{"__type":"TimeZoneDefinitionType:#Exchange","Id":"Tokyo Standard Time"}}},"Body":{"__type":"GetItemRequest:#Exchange","ItemShape":{"__type":"ItemResponseShape:#Exchange","BaseShape":"IdOnly"},"ItemIds":[{"__type":"ItemId:#Exchange","Id":"${emailId}"}],"ShapeName":"MessageDetails"}}`,
+        "x-req-source": "Mail"
+      }
+    })
+    let j = await res.json();
+    let rawEmailHeader: string;
+    try {
+      rawEmailHeader = j["Body"]["ResponseMessages"]["Items"][0]["Items"][0]["ExtendedProperty"][0]["Value"];
+    } catch (e) {
+      emails.push({
+        "ele": threadList[Number(index)],
+        "id": emailId,
+        "status": null
+      })
+      continue
+    }
+
+    let parsed = mailParser(rawEmailHeader);
+    let emailStat: SecStatus = parseSecStat(parsed["Authentication-Results"].slice(-1)[0], parsed["Received"]);
+    emails.push({
+      "ele": threadList[Number(index)],
+      "id": emailId,
+      "status": emailStat
+    })
+    await sleep(0.1);
+  }
+  return emails;
+}
+
+function inMail() { }
+
+function insertImg(dist: Element, stat: SecStatus) {
+  // authの結果
+  let authImgEle = document.createElement("img");
+  let authImg: string;
+  authImgEle.className = "auth-result";
+
+  if (!dist.querySelector(".auth-result")) {
+    let authResult: boolean = Object.keys(stat.auth).length ? true : false;
+    let authStr: string = "";
+    Object.keys(stat.auth).map(key => {
+      if (stat.auth[key].result.match(/^pass$|^bestguesspass$/)) {
+        authResult &&= true
+      } else {
+        authResult &&= false
+      }
+      authStr += `${key}: ${stat.auth[key].result};\n`
+    })
+
+    if (authResult) {
+      authImg = "img/verified.png";
+      authImgEle.setAttribute("title", `このメールには署名があります\n${authStr}`);
+    } else if (Object.keys(stat.auth).length === 0) {
+      authImg = "img/notverified.png";
+      authImgEle.setAttribute("title", `このメールには署名がついていません`);
+    } else {
+      authImg = "img/notverified.png";
+      authImgEle.setAttribute("title", `このメールの検証は失敗しています\n${authStr}`);
+    }
+    authImgEle.src = chrome.extension.getURL(authImg);
+    authImgEle.height = 18;
+    authImgEle.width = 18;
+    dist.prepend(authImgEle);
+  }
+
+  // 暗号化の結果
+  let encryptImgEle = document.createElement("img");
+  let lockImg: string;
+  encryptImgEle.className = "encrypt-result";
+
+  if (!dist.querySelector(".encrypt-result")) {
+    if (stat.encrypt.bool) {
+      lockImg = "img/lock.png";
+      encryptImgEle.setAttribute("title", `このメールは次のアルゴリズムで暗号化されて届きました\n${stat.encrypt.description}`);
+    } else {
+      lockImg = "img/notlock.png";
+      encryptImgEle.setAttribute("title", "このメールは暗号化されずに届きました");
+    }
+    // encryptImgEle.src = browser.extension.getURL(lockImg);
+    encryptImgEle.src = chrome.extension.getURL(lockImg);
+    encryptImgEle.height = 20;
+    encryptImgEle.width = 20;
+    dist.prepend(encryptImgEle);
+  }
+}
+
+async function inbox() {
+  await sleep(2);
+  let emails = await getEmailList();
+  for (let email of emails) {
+    console.log(email.status);
+    if (email.status === null) {
+      continue;
+    }
+    insertImg(email.ele, email.status);
+  }
+}
+
+
+export { inbox }
